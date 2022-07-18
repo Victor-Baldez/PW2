5a8461b1ca9a9da88b2f674d615f3077f2f72022
Victor-Baldez <vhbdc@aluno.ifnmg.edu.br>
2022-07-18 00:17:43 -0300

    marcando pares

--- atividade2/diasDaSemana/diasSemana.js
+++ atividade2/diasDaSemana/diasSemana.js
@@ -1,14 +1,14 @@
 function avancarDias() {
   let dia = Number(window.prompt("Informe o dia da semana: "))
-  let quantidade = Number(window.prompt("Informe a quantidade de dias que dejeja avançar: "))
-  let diasSemana = ["Segunda-feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
+  let quantidade = Number(window.prompt("Informe a quantidade de dias que deseja avançar: "))
+  let diasSemana = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
 
   let indiceDia = diasSemana.indexOf(dia);
 
   let indiceAtualizado = indiceDia + quantidade;
-  while (indiceAtualizado >= diasSemana.length) {
-    indiceAtualizado -= diasSemana.length;
+  while (indiceAtualizado >= 7) {
+    indiceAtualizado -= 7;
   }
 
-  alert (diasSemana[indiceAtualizado]);
+  alert ("Você avançou "+quantidade+" dias para "+diasSemana[indiceAtualizado]);
 }
