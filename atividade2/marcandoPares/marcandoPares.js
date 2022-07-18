bc75caf4518736f62c10c0339f08e4b795c05352
Victor-Baldez <vhbdc@aluno.ifnmg.edu.br>
2022-07-18 00:36:18 -0300

    1 file modified

--- atividade2/marcandoPares/marcandoPares.js
+++ atividade2/marcandoPares/marcandoPares.js
@@ -7,7 +7,7 @@ function contar() {
       saida.innerHTML += "<strong>"+cont+"</strong>&#x1F449;"
     }
     else {
-      saida.innerHTML += cont+" &#x1F449; "
+      saida.innerHTML += cont+" &#x1F449; " 
     }
     cont ++
   }
